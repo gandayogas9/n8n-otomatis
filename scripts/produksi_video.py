acd6034bc496d039ed948982c16412820b6191e5
gandayogas9 <gandayogas9@gmail.com>
2026-09-17 18:40:12 +0700

    Update produksi_video.py

--- scripts/produksi_video.py
+++ scripts/produksi_video.py
@@ -90,7 +90,7 @@ def main():
                 # 4. Upload hasil ke Google Drive
                 metadata = {
                     "name": f"{row_id}_final.mp4",
-                    "parents": [os.environ["12ynhDl-NHdwjFlYdR9e73rkF5qryMl1Q"]],
+                    "parents": [os.environ["DRIVE_FOLDER_ID"]],
                 }
                 media = MediaFileUpload(final_path, mimetype="video/mp4")
                 uploaded = drive_service.files().create(
